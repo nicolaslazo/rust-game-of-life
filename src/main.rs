c002d55b43ad05b08063513dcbc716e0a6c37dc3
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2022-02-25 01:53:10 -0300

    Implement basic app GUI layout

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,85 @@
 use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::{io, thread, time::Duration};
+use std::{
+    error, io,
+    sync::mpsc,
+    thread,
+    time::{Duration, Instant},
+};
 use tui::{
     backend::CrosstermBackend,
+    layout::Layout,
+    layout::{Constraint, Direction},
     widgets::{Block, Borders},
     Terminal,
 };
 
-fn main() -> Result<(), io::Error> {
-    enable_raw_mode()?;
+enum GameEvent<I> {
+    Input(I),
+    Tick,
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    enable_raw_mode().expect("Can enter raw mode");
 
     let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture).expect("Can capture mouse input");
     let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    let mut terminal = Terminal::new(backend).expect("Terminal is instantiated");
 
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default().title("Block").borders(Borders::ALL);
-        f.render_widget(block, size);
-    })?;
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(250);
+    thread::spawn(move || -> ! {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
 
-    thread::sleep(Duration::from_millis(2000));
+            if event::poll(timeout).expect("Events are properly polled") {
+                if let Event::Key(key) = event::read().expect("Key inputs are detected") {
+                    tx.send(GameEvent::Input(key))
+                        .expect("GameEvent inputs can be sent to the consumer");
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate && tx.send(GameEvent::Tick).is_ok() {
+                last_tick = Instant::now()
+            }
+        }
+    });
+
+    let mut exit = false;
+
+    loop {
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(2)
+                .constraints([Constraint::Percentage(85), Constraint::Percentage(15)].as_ref())
+                .split(f.size());
+            let block = Block::default().borders(Borders::ALL);
+            f.render_widget(block, chunks[0]);
+            let block = Block::default().borders(Borders::ALL);
+            f.render_widget(block, chunks[1]);
+
+            match rx.recv().unwrap() {
+                GameEvent::Input(event) => {
+                    if let KeyCode::Char('q') = event.code {
+                        exit = true
+                    }
+                }
+                GameEvent::Tick => {}
+            }
+        })?;
+
+        if exit {
+            break;
+        }
+    }
 
     disable_raw_mode()?;
     execute!(
