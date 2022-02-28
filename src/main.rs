6b5de65583a2906842d68bcd73ab0116590ce232
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2022-02-28 00:16:57 -0300

    App reacts to window changes and resizes vec

--- src/main.rs
+++ src/main.rs
@@ -13,23 +13,59 @@ use tui::{
     backend::{Backend, CrosstermBackend},
     layout::Layout,
     layout::{Alignment, Constraint, Direction},
+    style::Color,
+    symbols::Marker,
     text::{Span, Spans},
-    widgets::{Block, Borders, Paragraph},
+    widgets::{
+        canvas::{Canvas, Points},
+        Block, Borders, Paragraph,
+    },
     Terminal,
 };
 
 enum GameEvent<T> {
     Input(T),
     Tick,
+    Resize(usize, usize),
 }
 
-struct App {}
+struct App {
+    state: Vec<Vec<bool>>,
+}
 
 impl App {
     fn new() -> App {
-        App {}
+        App {
+            // Kind of sucks how the app starts in an invalid state that shouldn't exist
+            // but we can't reserve any memory until the UI is initialised and we know our dimensions
+            state: Vec::new(),
+        }
     }
+
+    fn resize(&mut self, w: usize, h: usize) {
+        self.state = vec![vec![false; w]; h];
+    }
+
     fn on_tick(&mut self) {}
+
+    fn cells(&self) -> Vec<(f64, f64)> {
+        // I chose not to implement Iterator as it would require tracking state and everything.
+        // No fancy uses here this is just for the UI to know whick blocks to paint white
+        if self.state.is_empty() {
+            // Covers the initial struct state before first UI call, just in case
+            return Vec::<(f64, f64)>::new();
+        }
+        self.state
+            .iter()
+            .enumerate()
+            .flat_map(move |(row_i, row)| {
+                row.iter()
+                    .enumerate()
+                    .filter(|(_, cell)| **cell)
+                    .map(move |(cell_i, _)| (cell_i as f64, row_i as f64))
+            })
+            .collect()
+    }
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
@@ -65,62 +101,90 @@ fn run_app<B: Backend>(
 ) -> Result<(), Box<dyn error::Error>> {
     let (tx, rx) = mpsc::channel();
 
-    thread::spawn(move || handle_game_events(tx, tick_rate));
+    let thread_tx = tx.clone();
+    thread::spawn(move || handle_game_events(thread_tx, tick_rate));
+
+    let mut current_game_dimensions: Option<(u16, u16)> = None;
 
     let mut game_running = false;
     let mut exit = false;
 
     loop {
-        terminal
-            .draw(|f| {
-                let chunks = Layout::default()
-                    .direction(Direction::Horizontal)
-                    .margin(2)
-                    .constraints([Constraint::Percentage(85), Constraint::Percentage(15)].as_ref())
-                    .split(f.size());
-                let block = Block::default().borders(Borders::ALL);
-                f.render_widget(block, chunks[0]);
+        terminal.draw(|f| {
+            let chunks = Layout::default() // Basic layout
+                .direction(Direction::Horizontal)
+                .margin(2)
+                .constraints([Constraint::Percentage(85), Constraint::Percentage(15)].as_ref())
+                .split(f.size());
 
-                let run_pause_str = match &game_running {
-                    false => "  Run",
-                    true => "  Pause",
-                };
+            let new_game_dimensions = (chunks[0].width, chunks[0].height); // Check for resize in the UI side
+            if Some(new_game_dimensions) != current_game_dimensions
+                && tx
+                    .send(GameEvent::Resize(
+                        new_game_dimensions.0.into(),
+                        new_game_dimensions.1.into(),
+                    ))
+                    .is_ok()
+            {
+                current_game_dimensions = Some(new_game_dimensions);
+            }
 
-                let instructions = Paragraph::new(vec![
-                    Spans::from(vec![Span::raw(" [Left click]")]),
-                    Spans::from(vec![Span::raw("  Add cell")]),
-                    Spans::from(vec![Span::raw("")]),
-                    Spans::from(vec![Span::raw(" [Right click]")]),
-                    Spans::from(vec![Span::raw("  Delete cell")]),
-                    Spans::from(vec![Span::raw("")]),
-                    Spans::from(vec![Span::raw(" [Enter]")]),
-                    Spans::from(vec![Span::raw(run_pause_str)]),
-                    Spans::from(vec![Span::raw("")]),
-                    Spans::from(vec![Span::raw(" [-, +]")]),
-                    Spans::from(vec![Span::raw(format!(
-                        "  Tick rate = {}",
-                        &tick_rate.as_millis()
-                    ))]),
-                    Spans::from(vec![Span::raw("")]),
-                    Spans::from(vec![Span::raw(" [q]")]),
-                    Spans::from(vec![Span::raw("  Exit")]),
-                ])
-                .alignment(Alignment::Left)
-                .block(Block::default().borders(Borders::ALL).title("Controls"));
-                f.render_widget(instructions, chunks[1]);
+            let canvas = Canvas::default() // Game rendering
+                .marker(Marker::Block)
+                .block(Block::default().borders(Borders::ALL))
+                .paint(|ctx| {
+                    ctx.draw(&Points {
+                        coords: app.cells().as_slice(),
+                        color: Color::White,
+                    })
+                });
+            f.render_widget(canvas, chunks[0]);
 
-                match rx.recv().unwrap() {
-                    GameEvent::Input(event) => match event.code {
-                        KeyCode::Char('q') => exit = true,
-                        KeyCode::Char('+') => tick_rate += Duration::from_millis(10),
-                        KeyCode::Char('-') => tick_rate -= Duration::from_millis(10),
-                        KeyCode::Enter => game_running = !game_running,
-                        _ => {}
-                    },
-                    GameEvent::Tick => app.on_tick(),
-                }
-            })
-            .expect("Can draw to terminal");
+            let run_pause_str = match &game_running {
+                // Controls rendering
+                false => "  Run",
+                true => "  Pause",
+            };
+
+            let instructions = Paragraph::new(vec![
+                Spans::from(vec![Span::raw(" [Left click]")]),
+                Spans::from(vec![Span::raw("  Add cell")]),
+                Spans::from(vec![Span::raw("")]),
+                Spans::from(vec![Span::raw(" [Right click]")]),
+                Spans::from(vec![Span::raw("  Delete cell")]),
+                Spans::from(vec![Span::raw("")]),
+                Spans::from(vec![Span::raw(" [Enter]")]),
+                Spans::from(vec![Span::raw(run_pause_str)]),
+                Spans::from(vec![Span::raw("")]),
+                Spans::from(vec![Span::raw(" [-, +]")]),
+                Spans::from(vec![Span::raw(format!(
+                    "  Tick rate = {}",
+                    &tick_rate.as_millis()
+                ))]),
+                Spans::from(vec![Span::raw("")]),
+                Spans::from(vec![Span::raw(" [q]")]),
+                Spans::from(vec![Span::raw("  Exit")]),
+            ])
+            .alignment(Alignment::Left)
+            .block(Block::default().borders(Borders::ALL).title("Controls"));
+            f.render_widget(instructions, chunks[1]);
+
+            match rx.recv().unwrap() {
+                // GameEvent handler/consumer
+                GameEvent::Input(event) => match event.code {
+                    KeyCode::Char('+') => tick_rate += Duration::from_millis(10),
+                    KeyCode::Char('-') => tick_rate -= Duration::from_millis(10),
+                    KeyCode::Enter => game_running = !game_running,
+
+                    KeyCode::Char('q') => exit = true,
+
+                    _ => {}
+                },
+
+                GameEvent::Tick => app.on_tick(),
+                GameEvent::Resize(w, h) => app.resize(w, h),
+            }
+        })?;
 
         if exit {
             break;
@@ -130,6 +194,7 @@ fn run_app<B: Backend>(
 }
 
 fn handle_game_events(tx: Sender<GameEvent<event::KeyEvent>>, tick_rate: Duration) {
+    // Reads for inputs and generates ticks
     let mut last_tick = Instant::now();
 
     loop {
